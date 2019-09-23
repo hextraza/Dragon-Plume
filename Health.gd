4005e7744559080ae4fa60ac16b5708156d1fa64
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2019-09-23 13:05:23 -0700

    Move health

--- Health.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-onready var player = get_node("/root/World/Player")
-var max_width = 1366
-
-func _physics_process(delta):
-	self.scale = Vector2(player.health / float(player.max_health), 1)
